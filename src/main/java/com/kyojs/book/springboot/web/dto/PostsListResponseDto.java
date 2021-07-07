0e0565de7da39e22fcf3cfe4bc380338fa921053
tjsry0466 <39974627+tjsry0466@users.noreply.github.com>
2021-07-07 16:00:51 +0900

    start oauth

---
+++ src/main/java/com/kyojs/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.kyojs.book.springboot.web.dto;
+
+import com.kyojs.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modefiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modefiedDate = entity.getModifiedDate();
+    }
+}
